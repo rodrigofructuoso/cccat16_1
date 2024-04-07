aebae6e6f66d0b2b6ba947ffb60f21a02720c3df
Rodrigo Fructuoso <rofructuoso@gmail.com>
2024-04-07 17:49:10 -0300

    refactor: fix several code smells to improve code readbility

--- code_smells.md
+++ code_smells.md
@@ -1,8 +1,8 @@
 # Code smells
 
-- Ifs aninhados
-- Código replicado
+- ~~Ifs aninhados~~
+- ~~Código replicado~~
 - Mistura de responsabilidades
 - Tratamento de erro inadequado
 - Nomes ruins
-- Magic numbers
+- ~~Magic numbers~~
--- src/api.ts
+++ src/api.ts
@@ -5,65 +5,44 @@ import { validate } from "./validateCpf";
 const app = express();
 app.use(express.json());
 
+const INVALID_CAR_PLATE = -5;
+const USER_ALREAY_EXISTS = -4;
+const INVALID_USER_NAME = -3;
+const INVALID_EMAIL_ADDRESS = -2;
+const INVALID_CPF = -1;
+
+function isNameValid(fullName: string) {
+	return fullName.match(/[a-zA-Z] [a-zA-Z]+/);
+}
+
+function isEmailValid(email: string) {
+	return email.match(/^(.+)@(.+)$/);
+}
+
+function isCarPlateValid(carPlate: string) {
+	return carPlate.match(/[A-Z]{3}[0-9]{4}/);
+}
+
 app.post("/signup", async function (req, res) {
-	let result;
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
 		const id = crypto.randomUUID();
-
 		const [acc] = await connection.query("select * from cccat16.account where email = $1", [req.body.email]);
-		if (!acc) {
-
-			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (req.body.email.match(/^(.+)@(.+)$/)) {
-
-					if (validate(req.body.cpf)) {
-						if (req.body.isDriver) {
-							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
-
-			} else {
-				// invalid name
-				result = -3;
-			}
-
-		} else {
-			// already exists
-			result = -4;
-		}
-		if (typeof result === "number") {
-			res.status(422).send(result + "");
-		} else {
-			res.json(result);
-		}
+		if (acc) throw USER_ALREAY_EXISTS;
+		if (!isNameValid(req.body.name)) throw INVALID_USER_NAME;
+		if (!isEmailValid(req.body.email)) throw INVALID_EMAIL_ADDRESS;
+		if (!validate(req.body.cpf)) throw INVALID_CPF;
+		if (req.body.isDriver && !isCarPlateValid(req.body.carPlate)) throw INVALID_CAR_PLATE;
+		await connection.query("insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
+		const response = {
+			accountId: id
+		};
+		res.json(response);
+	} catch (error) {
+		res.status(422).send(String(error));
 	} finally {
 		await connection.$pool.end();
 	}
-});
+}); 
 
 app.listen(3000);
